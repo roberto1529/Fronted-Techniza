695b63b18086f3c5be7db060afc66a5d4211485d
Roberto Molina <rmolina1529@gmail.com>
2025-02-25 19:59:10 -0500

    25-02-2025
    reducción  de carga de modulos

--- src/app/pages/auth/slider/slider.component.ts
+++ src/app/pages/auth/slider/slider.component.ts
@@ -1,33 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { FloatLabelModule } from 'primeng/floatlabel';
-import { IconFieldModule } from 'primeng/iconfield';
-import { InputIconModule } from 'primeng/inputicon';
-import { DatePickerModule } from 'primeng/datepicker';
-import { InputOtpModule } from 'primeng/inputotp';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sub-slider',
   standalone: true,
-  imports: [ButtonModule, FloatLabelModule,IconFieldModule,InputIconModule,
-    DatePickerModule, InputOtpModule, CommonModule, ],
+  imports: [CommonModule],
   templateUrl: './slider.component.html',
   styleUrl: '../auth.component.scss',
 })
-export class SliderComponent implements OnInit {
-  icons:string= "pi pi-moon";
-  severity: 'success' | 'info' | 'warn' | 'danger' | 'help' | 'primary' | 'secondary' | 'contrast' | null | undefined = 'secondary';
-  theme: string='';
+export class SliderComponent {
+ theme: string='';
 
-  constructor(){
-
-  }
-
-
-
-  ngOnInit(): void {}
-
+  constructor(){}
 
 
 

c1cd84ed4f5bfdad873a46e4406565e0ffa56f00
Roberto Molina <rmolina1529@gmail.com>
2025-02-27 00:09:11 -0500

    27-02-2025
    implementado de peticones  a backend primario a nivel local

--- src/app/pages/auth/formulario/services/formulario.service.ts
+++ src/app/pages/auth/formulario/services/formulario.service.ts
@@ -7,30 +7,24 @@ import { EncryptionService } from '../../../../shared/encryption.interceptor';
   providedIn: 'root',
 })
 export class FormularioService {
-  ep: any = "";
+  endpoint: string = 'http://localhost:3000/auth/v1/'
 
   constructor(private http: HttpClient, private readonly cto: EncryptionService) {}
 
   public Auth_Service(data: any) {
-    const { host, port, modulo, version, rutas } = this.ep;
-    const endpoint = `${host}${port}${modulo}${version}${rutas.index}`;
-
+ 
     // Encriptar datos antes de enviarlos
     const encryptedData = {
       data: this.cto.encryptData(data)
     };
-    return this.http.post(endpoint, encryptedData);  // Enviar datos encriptados
+    return this.http.post(this.endpoint, encryptedData);  // Enviar datos encriptados
   }
 
   public Auth_Validar_token(data: any){
-
-    const { host, port, modulo, version, rutas } = this.ep;
-    const endpoint = `${host}${port}${modulo}${version}${rutas.token}`;
-
     // Encriptar datos antes de enviarlos
     const encryptedData = {
       data: this.cto.encryptData(data)
     };
-    return this.http.post(endpoint, encryptedData);  // Enviar datos encriptados
+    return this.http.post(this.endpoint, encryptedData);  // Enviar datos encriptados
   }
 }
